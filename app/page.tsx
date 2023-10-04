51caf4549be2b41b532704aca1a3f301c7e494f6
Supriyo Saha <mm.saha1982@gmail.com>
2023-10-04 10:45:06 +0530

    saerchbar created

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Searchbar from "@/components/Searchbar";
 import Image from "next/image";
 import React from "react";
 
@@ -28,7 +29,7 @@ const Home = () => {
               convert, engage, and retain more.
             </p>
 
-            {/* searchbar */}
+            <Searchbar />
           </div>
           {/* Hero carousal */}
         </div>
