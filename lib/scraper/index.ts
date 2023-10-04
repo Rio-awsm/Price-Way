e593899704a07bafc6d113bcaf15cedcbfb70603
Supriyo Saha <mm.saha1982@gmail.com>
2023-10-04 18:21:23 +0530

    amazon scraping done

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,8 +1,8 @@
 "use server";
 
 import axios from "axios";
-import * as cheerio from 'cheerio';
-import { log } from "util";
+import * as cheerio from "cheerio";
+import { extractCurrency, extractDescription, extractPrice } from "../utils";
 
 export async function scrapedAmazonProduct(url: string) {
   if (!url) return;
@@ -19,7 +19,7 @@ export async function scrapedAmazonProduct(url: string) {
       username: `${username}-session-${session_id}`,
       password,
     },
-    host: 'brd.superproxy.io',
+    host: "brd.superproxy.io",
     port,
     rejectUnauthorized: false,
   };
@@ -27,14 +27,64 @@ export async function scrapedAmazonProduct(url: string) {
   try {
     //fetch product page
     const response = await axios.get(url, options);
-    const $ = cheerio.load(response.data)
+    const $ = cheerio.load(response.data);
 
     //extract title
-    const title = $('#productTitle').text().trim();
-    console.log({title});
+    const title = $("#productTitle").text().trim();
+    const currentPrice = extractPrice(
+      $(".priceToPay span.a-price-whole"),
+      $(".a.size.base.a-color-price"),
+      $(".a-button-selected .a-color-base")
+    );
+
+    const originalPrice = extractPrice(
+      $("#priceblock_ourprice"),
+      $(".a-price.a-text-price span.a-offscreen"),
+      $("#listPrice"),
+      $("#priceblock_dealprice"),
+      $(".a-size-base.a-color-price")
+    );
+
+    const outOfStock =
+      $("#availability span").text().trim().toLowerCase() ===
+      "currently unavailable";
+
+    const images =
+      $("#imgBlkFront").attr("data-a-dynamic-image") ||
+      $("#landingImage").attr("data-a-dynamic-image") ||
+      "{}";
+
+    const imgUrls = Object.keys(JSON.parse(images));
+
+    const currency = extractCurrency($(".a-price-symbol"));
+
+    const discountRate = $(".savingsPercentage").text().replace(/[-%]/g, "");
+
+    const description = extractDescription($)
+
+    //construct  data object with scraped info
+    const data = {
+       url,
+       currency: currency || '$',
+       image: imgUrls[0],
+       title,
+       currentPrice: Number(currentPrice),
+       originalPrice: Number(originalPrice),
+       priceHistory: [],
+       discountRate: Number(discountRate),
+       category: 'category',
+       reviewsCount:100,
+      stars: 4.5,
+      isOutOfStock: outOfStock,
+      description,
+      lowestPrice: Number(currentPrice) || Number(originalPrice),
+      highestPrice: Number(originalPrice) || Number(currentPrice),
+      averagePrice: Number(currentPrice) || Number(originalPrice),
+    }
+
+    return data;
     
   } catch (error: any) {
     console.log(error);
   }
-
 }
